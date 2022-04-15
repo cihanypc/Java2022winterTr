867407ba75aa6365f87131a0f489a0ed764cd894
Cihan Yapici <ypcihan@gmail.com>
2022-04-15 18:33:34 +0200

    ikinci commit

---
+++ src/day_44_abstractClass_interface/C01_Abstract_Class.java
@@ -0,0 +1,11 @@
+package day_44_abstractClass_interface;
+
+public abstract class C01_Abstract_Class {
+
+    public abstract void motor();
+
+    public abstract void vites();
+
+    public void teker() {
+    }
+}
---
+++ src/day_44_abstractClass_interface/C02_Toyota.java
@@ -0,0 +1,12 @@
+package day_44_abstractClass_interface;
+
+public class C02_Toyota extends C01_Abstract_Class {
+
+    @Override
+    public void motor() {
+    }
+
+    @Override
+    public void vites() {
+    }
+}
